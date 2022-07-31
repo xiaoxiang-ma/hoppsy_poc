973e3cf2ccd73b03db583e69010af194248ddf09
max() <xm53@cornell.edu>
2022-07-30 22:08:02 -0400

    added umap to requirements

--- hoppsy.py
+++ hoppsy.py
@@ -4,6 +4,7 @@ import numpy as np
 import pickle
 
 import hdbscan
+import umap
 from numpy.linalg import norm
 
 from pyabsa.functional import ATEPCCheckpointManager
@@ -61,9 +62,6 @@ st.success("corpus_clusters loaded")
 if st.button('check load'):
     st.write(aspect_extractor)
     st.write(sentence_transformer)
-    # st.balloons()
-# else:
-#     st.write('...')
 
 ###############################
 st.write("# Update topic categories")
@@ -75,55 +73,6 @@ topic_categories = topics.split(", ")
 
 st.write(topic_categories)
 
-# topic_categories = [
-#     'service',
-#     'hygeine', 
-#     'appetizer', 
-#     'desserts',
-#     'drinks/bar', 
-#     'food',
-#     'dishes', 
-#     'atmosphere', 
-#     'menu', 
-#     'price', 
-#     'management',
-#     'staff'
-# ]
-# st.write(topic_categories)
-
-###############################
-# Assign clusters to topic categories
-###############################
-
-def embed(model, sentences):
-    embeddings = model.encode(sentences, show_progress_bar=False)
-    return embeddings
-
-def cosine_sim(A,B):
-    cosine = np.dot(A,B)/(norm(A)*norm(B))
-    return cosine
-
-
-topic_categories_embeddings = embed(sentence_transformer, topic_categories)
-clusters_embedding = embed(sentence_transformer, corpus_clusters.group.to_list())
-
-
-# Find cosine similarities
-similarities = []
-for i in range(len(clusters_embedding)):
-    similarities.append([cosine_sim(phrase, clusters_embedding[i]) for phrase in topic_categories_embeddings])
-similarities = pd.DataFrame(similarities, columns = topic_categories)
-similarities['topic'] = similarities.idxmax(axis=1)
-similarities['topic_strength'] = similarities.max(axis=1)
-similarities = similarities.round(2)
-
-# Find label strength and correct "-1"
-label_strength = pd.concat([corpus_clusters, similarities[['topic','topic_strength']]], axis=1)
-
-label_strength_dict = dict(zip(label_strength['label_st1'] ,label_strength['topic_strength']))
-label_dict = dict(zip(label_strength['label_st1'] ,label_strength['topic']))
-label_dict[-1] = "_Generic"
-
 ###############################
 st.write("# Enter reviews")
 ###############################
@@ -133,49 +82,85 @@ The pistachio icecream was a delight! Service was ok'''
 user_review = st.text_area('Text to analyze', sample_review).split("\n")
 st.write(user_review)
 
+if st.button('Compute Insights'):
 
-###############################
-# ABSA on reviews 
-###############################
+    ###############################
+    # Assign clusters to topic categories
+    ###############################
 
-atepc_result = aspect_extractor.extract_aspect(inference_source=user_review,
-                                               save_result=False,
-                                               print_result=True,  # print the result
-                                               pred_sentiment=True,  # Predict the sentiment of extracted aspect terms
-                                               )
+    def embed(model, sentences):
+        embeddings = model.encode(sentences, show_progress_bar=False)
+        return embeddings
 
-for i in range(len(atepc_result)):
-    atepc_result[i]['raw_text'] = user_review[i]
+    def cosine_sim(A,B):
+        cosine = np.dot(A,B)/(norm(A)*norm(B))
+        return cosine
 
-reviews_absa = []
-for i in atepc_result:
-    for j in range(len(i['aspect'])):
-        reviews_absa.append([
-            i['raw_text'],
-            i['aspect'][j],
-            i['sentiment'][j],
-            i['confidence'][j]
-        ])
-reviews_absa = pd.DataFrame(reviews_absa)
-reviews_absa.columns = ['text', 'aspect', 'sentiment', 'confidence']
-st.write(reviews_absa)
 
-# embed aspects, umap dim reduce, and find which clusters they belong
-inference_aspects = reviews_absa['aspect'].tolist()
-inference_aspects_embeddings = embed(sentence_transformer, inference_aspects)
-inference_aspects_embeddings_umap = umap_reducer.transform(inference_aspects_embeddings)
-test_labels, strengths = hdbscan.approximate_predict(hdbscan_clusters, inference_aspects_embeddings_umap)
+    topic_categories_embeddings = embed(sentence_transformer, topic_categories)
+    clusters_embedding = embed(sentence_transformer, corpus_clusters.group.to_list())
 
-reviews_absa['topic'] = [label_dict[i] for i in test_labels]
-reviews_absa['topic_strength'] = [label_strength_dict[i] for i in test_labels]
-reviews_absa.confidence = reviews_absa.confidence.round(2)
 
-# Prettify
-df_display = reviews_absa.groupby(['text','sentiment']).agg(
-    aspect=('aspect', lambda x: list(x)),
-    aspect_topic=('topic', lambda x: list(x)),
-    sentiment_confidence=('confidence', lambda x: list(x)),
-    topic_confidence=('topic_strength', lambda x: list(x)),
-)
+    # Find cosine similarities
+    similarities = []
+    for i in range(len(clusters_embedding)):
+        similarities.append([cosine_sim(phrase, clusters_embedding[i]) for phrase in topic_categories_embeddings])
+    similarities = pd.DataFrame(similarities, columns = topic_categories)
+    similarities['topic'] = similarities.idxmax(axis=1)
+    similarities['topic_strength'] = similarities.max(axis=1)
+    similarities = similarities.round(2)
 
-st.write(df_display)
+    # Find label strength and correct "-1"
+    label_strength = pd.concat([corpus_clusters, similarities[['topic','topic_strength']]], axis=1)
+
+    label_strength_dict = dict(zip(label_strength['label_st1'] ,label_strength['topic_strength']))
+    label_dict = dict(zip(label_strength['label_st1'] ,label_strength['topic']))
+    label_dict[-1] = "_Generic"
+
+
+
+    ###############################
+    # ABSA on reviews 
+    ###############################
+
+    atepc_result = aspect_extractor.extract_aspect(inference_source=user_review,
+                                                save_result=False,
+                                                print_result=True,  # print the result
+                                                pred_sentiment=True,  # Predict the sentiment of extracted aspect terms
+                                                )
+
+    for i in range(len(atepc_result)):
+        atepc_result[i]['raw_text'] = user_review[i]
+
+    reviews_absa = []
+    for i in atepc_result:
+        for j in range(len(i['aspect'])):
+            reviews_absa.append([
+                i['raw_text'],
+                i['aspect'][j],
+                i['sentiment'][j],
+                i['confidence'][j]
+            ])
+    reviews_absa = pd.DataFrame(reviews_absa)
+    reviews_absa.columns = ['text', 'aspect', 'sentiment', 'confidence']
+    st.write(reviews_absa)
+
+    # embed aspects, umap dim reduce, and find which clusters they belong
+    inference_aspects = reviews_absa['aspect'].tolist()
+    inference_aspects_embeddings = embed(sentence_transformer, inference_aspects)
+    inference_aspects_embeddings_umap = umap_reducer.transform(inference_aspects_embeddings)
+    test_labels, strengths = hdbscan.approximate_predict(hdbscan_clusters, inference_aspects_embeddings_umap)
+
+    reviews_absa['topic'] = [label_dict[i] for i in test_labels]
+    reviews_absa['topic_strength'] = [label_strength_dict[i] for i in test_labels]
+    reviews_absa.confidence = reviews_absa.confidence.round(2)
+
+    # Prettify
+    df_display = reviews_absa.groupby(['text','sentiment']).agg(
+        aspect=('aspect', lambda x: list(x)),
+        aspect_topic=('topic', lambda x: list(x)),
+        sentiment_confidence=('confidence', lambda x: list(x)),
+        topic_confidence=('topic_strength', lambda x: list(x)),
+    )
+
+    st.write(df_display)
